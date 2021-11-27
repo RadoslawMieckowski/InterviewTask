b7c2c8c922487c4a038af44b3339d3842c578d9d
Radosław Mieckowski <rmieckowski@wp.pl>
2021-11-27 17:02:44 +0100

    create readAndMakeMap() method in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException{
         genrateAndWrite();
+        readAndMakeMap();
     }
     public static void genrateAndWrite() throws FileNotFoundException {
         long stamp=1615560000; // or: System.currentTimeMillis(); to get a real-time result
@@ -21,9 +25,43 @@ public class Main {
         }
         file.close();
     }
-    //data.put(Long.valueOf(stamp),Integer.valueOf(input)); put pair to the map, przy sładaniu ,mapy powinno  być przd obliczeniem stampa
-    public static void readAndMakeMap(){
+    public static void readAndMakeMap() throws IOException {
+        Scanner in=new Scanner(new File("src/data.txt"));
+        //var contents=new String(Files.readAllBytes(Paths.get("src/data.txt")), StandardCharsets.UTF_8); nigdy nie używaj tego  ze strumieniami!!
+        List<String>words=new LinkedList<>();
+        while(in.hasNext()){
+            String word=in.next();
+            words.add(word);
+        }
+
+        List<Long> stamps=words
+                .stream()
+                .filter(x->x.endsWith(":"))
+                .map(x->x.replace(":",""))
+                .map(x->Long.parseLong(x))
+                //.mapToLong(x -> Long.parseLong(x)) użycie tego uniemożliwia użycie .collect(Collectors.toList())
+                .collect(Collectors.toList());
+        for(Long x :stamps){
+            System.out.println(x);
+        }
+
+
+        List<Integer> inputs=words
+                .stream()
+                .filter(x->x.length()==1)
+                .map(x->Integer.parseInt(x))
+                .collect(Collectors.toList());
+        for(int x :inputs){
+            System.out.println(x);
+        }
+
         var data=new TreeMap<Long,Integer>();
+        ListIterator<Long>stampsLi=stamps.listIterator();
+        ListIterator<Integer>inputsLi=inputs.listIterator();
+
+        while(stampsLi.hasNext()){
+            data.put(stampsLi.next(), inputsLi.next());
+        }
         data.forEach((k,v)-> System.out.println(k +": "+v));
     }
 
