5a4f0f498dbe0e95b197a6b0d7dc422e6f3e3b5a
Radosław Mieckowski <rmieckowski@wp.pl>
2021-11-27 17:10:38 +0100

    add comments in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,84 +10,75 @@ public class Main {
     public static void main(String[] args) throws IOException{
         genrateAndWrite();
         TreeMap<Long,Integer>data= readAndMakeMap();
-        //data.forEach((k,v)-> System.out.println(k +": "+v));
-        //System.out.println();
         TreeMap<Long,Integer>processedData=makeTreeMapOfChanges(data);
-        //processedData.forEach((k,v)-> System.out.println(k+": "+v));
-        Stream<Map.Entry<Long,Integer>> outputStream =processedData.entrySet()
+        Stream<Map.Entry<Long,Integer>> outputStream =processedData         //creating stream of map's entries
+                .entrySet()
                 .stream();
-        outputStream.forEach(e-> System.out.println(e.getKey()+": "+e.getValue()));
+        outputStream.forEach(e-> System.out.println(e.getKey()+": "+e.getValue()));         //display outputStream's view
 
 
     }
     public static void genrateAndWrite() throws FileNotFoundException {
-        long stamp=1615560000; // or: System.currentTimeMillis(); to get a real-time result
-        int sec;
-        int input;
-        var file=new PrintWriter("src/data.txt");
+        long stamp=1615560000;           // or: System.currentTimeMillis(); to get a real-time result
+        int sec;                        // random seconds
+        int input;                        //value 0 or 1, picked randomly
+        var file=new PrintWriter("src/data.txt");           //where to print data
         for(int i=0;i<10;i++){
             double inputDouble=Math.random();
             if(inputDouble<0.5)input=0;
             else input=1;
             file.println(stamp + ": "+input);
-            sec=(int)(Math.random()*5+(11-5));//values from 5 to 10
-            stamp+=sec;
+            sec=(int)(Math.random()*5+(11-5));              //values from 5 to 10
+            stamp+=sec;                                     //updating variable stamp
         }
         file.close();
     }
     public static TreeMap<Long,Integer> readAndMakeMap() throws IOException {
-        Scanner in=new Scanner(new File("src/data.txt"));
-        //var contents=new String(Files.readAllBytes(Paths.get("src/data.txt")), StandardCharsets.UTF_8); nigdy nie używaj tego  ze strumieniami!!
-        List<String>words=new LinkedList<>();
-        while(in.hasNext()){
+        Scanner in=new Scanner(new File("src/data.txt"));            //source to read
+        List<String>words=new LinkedList<>();                   //list of all words in the source
+        while(in.hasNext()){                                // reading from source
             String word=in.next();
-            words.add(word);
+            words.add(word);                                //filling the words List
         }
 
-        List<Long> stamps=words
+        List<Long> stamps=words                                 //creating list of stamps
                 .stream()
-                .filter(x->x.endsWith(":"))
-                .map(x->x.replace(":",""))
-                .map(x->Long.parseLong(x))
-                //.mapToLong(x -> Long.parseLong(x)) użycie tego uniemożliwia użycie .collect(Collectors.toList())
-                .collect(Collectors.toList());
-      /*  for(Long x :stamps){
-            System.out.println(x);
-        }*/
+                .filter(x->x.endsWith(":"))                     //filtring stamps
+                .map(x->x.replace(":",""))      //cutting unnessesary characters ":"
+                .map(x->Long.parseLong(x))                      //converting Strings to Longs
+                .collect(Collectors.toList());                  //filling the stamps List
 
 
-        List<Integer> inputs=words
+        List<Integer> inputs=words                      //creating list of inputs
                 .stream()
-                .filter(x->x.length()==1)
-                .map(x->Integer.parseInt(x))
-                .collect(Collectors.toList());
-       /* for(int x :inputs){
-            System.out.println(x);
-        }*/
+                .filter(x->x.length()==1)                       //filtring inputs
+                .map(x->Integer.parseInt(x))                    //converting Strings to Integers
+                .collect(Collectors.toList());                  // //filling the inputs List
 
-        var data=new TreeMap<Long,Integer>();
-        ListIterator<Long>stampsLi=stamps.listIterator();
-        ListIterator<Integer>inputsLi=inputs.listIterator();
+
+        var data=new TreeMap<Long,Integer>();                   //creating map with stamps as keys and inputs as values
+        ListIterator<Long>stampsLi=stamps.listIterator();       //creating listIterator for stamps
+        ListIterator<Integer>inputsLi=inputs.listIterator();    //creating listIterator for inputs
 
         while(stampsLi.hasNext()){
-            data.put(stampsLi.next(), inputsLi.next());
+            data.put(stampsLi.next(), inputsLi.next());         // filling the data Map
         }
 
-        return data;
+        return data;                                            //return TreeMap
     }
     public static TreeMap<Long,Integer> makeTreeMapOfChanges(TreeMap<Long,Integer> treeMap){
 
-        int previousInput=2;
-        TreeMap<Long,Integer>processedMap=new TreeMap<>();
-        Iterator<Map.Entry<Long, Integer>> treeMapIt = treeMap.entrySet().iterator();
+        int previousInput=2;                                           //variable for monitorng values
+        TreeMap<Long,Integer>processedMap=new TreeMap<>();              //create map for results
+        Iterator<Map.Entry<Long, Integer>> treeMapIt = treeMap.entrySet().iterator();   // create interator for TreeMap<Long, Integer> parameter
         while (treeMapIt.hasNext()) {
-            Map.Entry<Long, Integer> entry = treeMapIt.next();
-            if (entry.getValue()!= previousInput){
-                processedMap.put(entry.getKey(),entry.getValue());
-                previousInput =entry.getValue();
-            }
+            Map.Entry<Long, Integer> entry = treeMapIt.next();              //go to the next entry
+            if (entry.getValue()!= previousInput){                        //if prevous value is different,
+                processedMap.put(entry.getKey(),entry.getValue());        //put entry to the results map
+                previousInput =entry.getValue();                          // set previousInput to the value
+            }                                                              //of entry
         }
-        return processedMap;
+        return processedMap;                    //return results
     }
 
 
